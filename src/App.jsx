9cf1ad85fc8e03c3ae2a69582bcf14007342097a
abdulmalikgh <musah.abdulmalikgh@gmail.com>
2024-08-31 11:10:14 +0000

    Add Top bar and side bar compoents

---
+++ src/App.jsx
@@ -0,0 +1,186 @@
+import { useState } from "react";
+import './assets/scrollbar.css'
+
+function App() {
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+  return (
+    <div className="min-h-screen flex flex-col">
+    {/* Header */}
+    <header className="h-[80px] border-b border-customGray">
+      <div className="container mx-auto flex items-center h-full">
+        <h1 className="text-lg font-bold ">
+          <img src="../public/logo.svg" alt="" />
+        </h1>
+        {/* Input here */}
+        <div className="w-full flex items-center justify-center px-5">
+        <div className="relative w-full max-w-[598px]">
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            <img src="../public/fi-br-search.svg" alt="Search Icon" />
+          </div>
+
+          <input 
+            type="text" 
+            className="pl-10 pr-16 h-[40px] rounded-[8px] border border-[#B4B4B4] bg-[#F5F5F5] w-full focus:outline-none focus:ring-0 box-border placeholder-gray-400" 
+            placeholder="Search Creative OS" 
+          />
+
+          <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500">
+            ⌘ + K
+          </div>
+        </div>
+        </div>
+      </div>
+    </header>
+
+     
+     <main className="">
+        <div className="flex">
+          {/* Sidebar */}
+          <div
+            className={`fixed top-[80px] left-0 h-[calc(100vh-80px)] bg-[#F5F5F5] border-r border-[#B4B4B4] transition-transform transform ${
+              isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
+            } w-64 p-5 overflow-y-auto`}
+          >
+            <div className="nav-content">
+              <ul>
+                <li className="cursor-pointer">
+                  <div className="flex items-center ps-3 py-2.5 font-semibold text-[14px] leading-[16.94px]">
+                    <div className="flex items-center w-full gap-3">
+                      <img src="../public/compass.svg" alt="" />
+                      Inspirations
+                    </div>
+                    
+                    <button className="py-5 ps-5" onClick={toggleSidebar}>
+                    <img src="../public/btn-nav-chevron.svg" alt="" />
+                    </button>
+                  </div>
+                </li>
+                <li className="cursor-pointer">
+                  <div className="flex items-center ps-3 py-2.5 font-semibold text-[14px] leading-[16.94px]">
+                    <div className="flex items-center w-full gap-3">
+                      <img src="../public/templates.svg" alt="" />
+                      Templates
+                    </div>
+                  </div>
+                </li>
+                <li className="cursor-pointer">
+                  <div className="flex items-center ps-3 py-2.5 font-semibold text-[14px] leading-[16.94px]">
+                    <div className="flex items-center w-full gap-3">
+                      <img src="../public/collections.svg" alt="" />
+                      Collections
+                    </div>
+                  </div>
+                </li>
+                <li className="cursor-pointer">
+                  <div className="flex items-center ps-3 py-2.5 font-semibold text-[14px] leading-[16.94px] rounded-lg border border-[#A259FF]">
+                    <div className="flex items-center w-full gap-3">
+                      <img src="../public/brands.svg" alt="" />
+                      Brands
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className="border-b border-customGray w-[190px] my-5"></div>
+            <div className="">
+              <h3 className="text-textMuted text-xs">FEATURED</h3>
+              <div className="">
+                <ul>
+                  <li className="flex items-center gap-5 text-sm py-2 px-5 cursor-pointer">
+                    <span>Hidden gems</span>
+                    <img src="../public/tag-pro.svg" alt="" />
+                  </li>
+                  <li className="flex items-center gap-5 text-sm py-2 px-5 cursor-pointer">
+                    <span>Evergreen ads</span>
+                    <img src="../public/tag-pro.svg" alt="" />
+                  </li>
+                  <li className="flex items-center gap-5 text-sm py-2 px-5 cursor-pointer">
+                    <span>Testing lab</span>
+                    <img src="../public/tag-pro.svg" alt="" />
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="border-b border-customGray w-[190px] my-5"></div>
+            <div className="mb-10">
+              <div className="flex justify-between mb-3">
+                <h4 className="text-sm text-textMuted">Saved</h4>
+                <button>
+                  <img src="../public/add-collection.svg" alt="" />
+                </button>
+              </div>
+
+              <div className="ms-5 h-[220px] overflow-auto scrollbar-left">
+                  <ul>
+                    <li className="cursor-pointer text-sm py-2.5 px-5">
+                      All Saved
+                    </li>
+                    <li className="cursor-pointer text-sm flex justify-between py-2 px-5">
+                      <span>Board Name</span>
+                      <img src="../public/dots-expand.svg" alt="" />
+                    </li>
+                    <li className="cursor-pointer text-sm flex justify-between py-2 px-5">
+                      <span>Board Name</span>
+                      <img src="../public/dots-expand.svg" alt="" />
+                    </li>
+                    <li className="cursor-pointer text-sm flex justify-between py-2 px-5">
+                      <span>Board Name</span>
+                      <img src="../public/dots-expand.svg" alt="" />
+                    </li>
+                    <li className="cursor-pointer text-sm flex justify-between py-2 px-5">
+                      <span>Board Name</span>
+                      <img src="../public/dots-expand.svg" alt="" />
+                    </li>
+                    <li className="cursor-pointer text-sm flex justify-between py-2 px-5">
+                      <span>Board Name</span>
+                      <img src="../public/dots-expand.svg" alt="" />
+                    </li>
+                    <li className="cursor-pointer text-sm flex justify-between py-2 px-5">
+                      <span>Board Name</span>
+                      <img src="../public/dots-expand.svg" alt="" />
+                    </li>
+                    <li className="cursor-pointer text-sm flex justify-between py-2 px-5">
+                      <span>Board Name</span>
+                      <img src="../public/dots-expand.svg" alt="" />
+                    </li>
+                  </ul>
+              </div>
+              <div className="text-sm mt-2">View all boards</div>
+            </div>
+            <div className="border-b border-customGray w-[190px] my-5"></div>
+            <div className="">
+              <ul>
+                <li className="cursor-pointer text-base pb-2">Settings</li>
+                <li className="cursor-pointer text-base pb-2">Help & Feedback</li>
+                <li className="cursor-pointer text-base pb-2">Join the CreativeOS Slack</li>
+              </ul>
+            </div>
+            <div className="border-b border-customGray w-[190px] my-5"></div>
+            <div className="flex items-center gap-5">
+              <div className="h-10 w-10 rounded-full bg-customPurple"></div>
+              <p>My Account</p>
+            </div>
+          </div>
+
+          {/* Main Content */}
+          <div className="flex-1 p-5">
+            <button
+              className="bg-green-500 text-white py-2 px-4 rounded"
+              onClick={toggleSidebar}
+            >
+              {isSidebarOpen ? 'Hide Sidebar' : 'Show Sidebar'}
+            </button>
+            <div className="mt-5">Main Content Here</div>
+          </div>
+        </div>
+      </main>
+
+       
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customGray: '#B4B4B4',
+        customPurple: '#A259FF',
+        textMuted: '#7F7F7F'
+      },
+    },
+  },
+  plugins: [],
+}
+
